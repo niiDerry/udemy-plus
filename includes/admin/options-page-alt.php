49e5f2aed2188511d3af0ed339ef5e71fd4e81c4
Derrick Angelo Tagoe <derricktagoe@gmail.com>
2025-10-09 13:25:48 -0400

    Adding sections

--- includes/admin/options-page-alt.php
+++ includes/admin/options-page-alt.php
@@ -5,7 +5,9 @@ function up_plugins_options_alt_page() {
     <div class="wrap">
       <form method="POST" action="options.php">
         <?php 
-          settings_fields('up_options_group')
+          settings_fields('up_options_group');
+          do_settings_sections('up_options_page');
+          submit_button();
         ?>
       </form>
       
