5aecbc38b15fdd9a02714ef2e70c82beacf279ab
Derrick Angelo Tagoe <derricktagoe@gmail.com>
2025-10-09 13:25:57 -0400

    Adding sections

--- includes/admin/settings-api.php
+++ includes/admin/settings-api.php
@@ -2,4 +2,15 @@
 
 function up_settings_api() {
   register_setting( 'up_options_group', 'up_options' );
+
+  add_settings_section( 
+    'up_options_section',
+    __('Udemy Plus Settings', 'udemy-plus'),
+    'up_options_section_cb',
+    'up_options_page'
+  );
+}
+
+function up_options_section_cb() {
+  ?> <p>An alternative form for updating options with the settings API</p> <?php
 }
