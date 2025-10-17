5f343cbb822e37998504b8c6256944391b579180
Derrick Angelo Tagoe <derricktagoe@gmail.com>
2025-10-17 14:20:02 -0400

    uninstalling a plugin

---
+++ uninstall.php
@@ -0,0 +1,13 @@
+<?php 
+
+if ( !defined( "WP_UNINSTALL_PLUGIN" ) ) {
+  exit();
+}
+
+delete_option( "up_options");
+
+global $wpdb;
+$wpdb->query(
+  "DROP TABLE IF EXISTS {$wpdb->prefix}recipe_rating" 
+);
+// Drop the custom database table for recipe ratings
